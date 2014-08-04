a6b529a0914c6e867066ceb2190f306d9868e65b
Aaron Marriner <amarriner@amarriner.com>
2014-08-04 20:26:26 +0000

    Moved POS object to separate file for clarity, improved logic for articles

--- question.py
+++ question.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 
 """Script to ask silly questions?"""
-
 from nltk.corpus import brown
-from pattern.en import wordnet
-from string import Template
+from pattern.en import conjugate, referenced, singularize, wordnet
+from pos import POS
 
 import cache_object
 import nltk
@@ -15,36 +14,23 @@ import re
 import sys
 
 
-# List of potential questions to pull from 
-QUESTIONS = ['If you <VBN> [A] <NN>, how would you <VB> it?']
+# List of potential questions to pull from
+QUESTIONS = [
+               'If <NN> <VBZ>, can <PN> <VB> it?',
+               'If you <VBN> <NN>, how would you <VB> it?',
+               'When would you <VB> <NN>?',
+               'How does one <VB> <NN> with <NN$|NN>?',
+               'How does one <VB> <NN> with <NN>?',
+               'Why does <PN> <VB>?',
+               'When <VBG> <NN>, who <VBZ> it?',
+               'Who <VBZ> the <AP> <NNS>?',
+               'Who is <VBG> the <NNS>?',
+            ]
+# Working directory
+PWD = '/home/amarriner/python/question/'
 
-# Used to replace parts of speech within a string 
+# Used to replace parts of speech within a string
 TAG_PATTERN = re.compile(r'(<.*?>)')
-
-# Parts of speech dict with various information about each, used in processing questions
-POS = {
-         'NN' : {'name'    : 'noun',
-                 'lexnames': [
-                                'noun.animal',
-                                'noun.body',
-                                'noun.food',
-                                'noun.plant',
-                                'noun.shape',
-                             ]
-                },
-         'VB' : {'name'    : 'verb',
-                 'lexnames': [
-                                'verb.body',
-                                'verb.communication',
-                                'verb.competition',
-                                'verb.contact',
-                                'verb.social',
-                             ]
-                },
-         'VBN': {'name'    : 'past-participle',
-                 'lexnames': []
-                },
-      }
 
 # Just a list of vowels...
 VOWELS = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
@@ -53,10 +39,10 @@ VOWELS = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
 def build_word_lists():
    """Run through the POS tags and get lists of words for each"""
 
-   # Grab a cached version if possible, otherwise load words from the brown corpus with the given tag into the 
+   # Grab a cached version if possible, otherwise load words from the brown corpus with the given tag into the
    # appropriate POS dict entry
    for tag in POS.keys():
-      filename = 'pos_' + tag + '.cache'
+      filename = PWD + 'cache/pos_' + tag + '.cache'
       if os.path.isfile(filename):
          POS[tag]['words'] = cache_object.load(filename)
       else:
@@ -64,7 +50,7 @@ def build_word_lists():
          cache_object.dump(POS[tag]['words'], filename)
 
 
-def get_random_word(t):
+def get_random_word(t, ref=False):
    """Return a random word from a set filtering on lexname category if necessary"""
 
    # If there are entries in the lexnames list for the given POS tag, limit results to that,
@@ -75,7 +61,11 @@ def get_random_word(t):
       lexname = ''
       while lexname not in POS[t]['lexnames']:
          word = random.choice(POS[t]['words'])[0]
-         synset = wordnet.synsets(word, pos=t)
+
+         if t == 'VBG':
+            synset = wordnet.synsets(conjugate(word), pos=t)
+         else:
+            synset = wordnet.synsets(get_singular(word), pos=t)
 
          if synset:
             lexname = synset[0].lexname
@@ -83,20 +73,16 @@ def get_random_word(t):
    else:
       word = random.choice(POS[t]['words'])[0]
 
-   return word
+   if ref:
+      word = referenced(word)
+
+   return word.lower()
 
 
-def replace_articles(question):
-   """Replaces [A] placeholder with 'a' or 'an'"""
+def get_singular(word):
+   """Further refinement of the pattern.en.singularize function"""
 
-   # Find the placeholder if it exists and check the word after that to determine whether to return a or an
-   if question.split(' ').index('[A]') >= 0:
-      if question.split(' ')[question.split(' ').index('[A]') + 1][:1] in VOWELS:
-         question = question.replace('[A]', 'an')
-      else:
-         question = question.replace('[A]', 'a')
-
-   return question
+   return singularize(word).replace('\'', '')
 
 
 def replace_pos(question):
@@ -109,14 +95,31 @@ def replace_pos(question):
 
          # Get a new iterator for the regex pattern. No terribly efficient, but since we're potentially
          # changing the length and indicies of the string on each pass, we have to get a new one every time
+         last = None
+         replace = ''
          tag = TAG_PATTERN.finditer(question).next()
-         t = tag.group().replace('<', '').replace('>', '')
-         if t in POS.keys():
+         (start, end) = tag.span()
+         for t in tag.group().replace('<', '').replace('>', '').split('|'):
 
             # If the POS tag we found exists in the POS tag, strip out the placeholder and replace it with a random word
-            word = get_random_word(t)
-            (start, end) = tag.span()
-            question =  question[:start] + word + question[end:]
+            if t in POS.keys():
+
+               # ref determines whether to place an article before the word or not
+               ref = POS[t]['ref']
+               if last:
+                  if POS[t]['ref'] and POS[last]['ref']:
+                     ref = False
+
+               word = get_random_word(t, ref)
+
+               if replace:
+                  replace = replace + ' '
+
+               replace = replace + word
+
+            last = t
+
+         question = question[:start] + replace + question[end:]
 
       except StopIteration, e:
          done = True
@@ -129,8 +132,11 @@ def main():
 
    build_word_lists()
 
-   print replace_articles(replace_pos(random.choice(QUESTIONS)))
-      
+   question = random.choice(QUESTIONS)
+   print question
+   print replace_pos(question)
+
 
 if __name__ == "__main__":
    sys.exit(main())
+
