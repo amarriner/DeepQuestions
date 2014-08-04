b01b140d3b3eacbc680ac512b3ed3148044769ad
Aaron Marriner <amarriner@amarriner.com>
2014-08-04 15:34:09 +0000

    Added skeleton script

---
+++ question.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+"""Script to ask silly questions?"""
+
+import sys
+
+
+def main():
+   """Main entry point"""
+   return 1
+
+
+if __name__ == "__main__":
+   sys.exit(main())
